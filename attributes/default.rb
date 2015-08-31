3d5b01511960aa904903fe9adc685a0d91854770
Maxim Filatov <pipopolam@gmail.com>
2015-08-31 23:45:13 +0300

    Attributes/Default: bump versions for etcd and flannel

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,6 @@ default['kubernetes']['version']               = 'v1.0.3'
 default['kubernetes']['interface']             = 'eth1'
 default['kubernetes']['cluster-name']          = 'kubernetes'
 default['kubernetes']['etcd']['discovery_url'] = ''
-default['kubernetes']['etcd']['version']       = 'v2.1.1'
-default['kubernetes']['flannel']['version']    = '0.5.2'
+default['kubernetes']['etcd']['version']       = 'v2.1.2'
+default['kubernetes']['flannel']['version']    = '0.5.3'
 default['kubernetes']['flannel']['network']    = {'Network' => '10.222.10.0/16'}
