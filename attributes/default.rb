2d36d3f928df2f4c5a0a8c82fb9c64e7bd37e08b
Maxim Filatov <pipopolam@gmail.com>
2015-11-07 14:26:49 +0300

    Versions bump

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
-default['kubernetes']['version']               = 'v1.0.6'
+default['kubernetes']['version']               = 'v1.0.7'
 default['kubernetes']['interface']             = 'eth1'
 default['kubernetes']['cluster-name']          = 'kubernetes'
 default['kubernetes']['cluster_dns']           = '10.222.222.222'
 default['kubernetes']['cluster_domain']        = 'kubernetes.local'
 default['kubernetes']['etcd']['discovery_url'] = ''
-default['kubernetes']['etcd']['version']       = 'v2.2.0'
-default['kubernetes']['flannel']['version']    = '0.5.3'
+default['kubernetes']['etcd']['version']       = 'v2.2.1'
+default['kubernetes']['flannel']['version']    = '0.5.4'
 default['kubernetes']['flannel']['network']    = {'Network' => '10.222.10.0/16'}
