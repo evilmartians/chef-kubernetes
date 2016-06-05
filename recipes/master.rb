06a4ff15019dde49750f2251656e11f454f291f0
Maxim Filatov <pipopolam@gmail.com>
2016-06-05 14:10:26 +0300

    K8S SSL files

--- recipes/master.rb
+++ recipes/master.rb
@@ -13,4 +13,8 @@ end
 
 end
 
+[:client_ca_file, :tls_cert_file, :tls_private_key_file].each do |f|
+  file node[:kubernetes][f] do
+    content Chef::EncryptedDataBagItem.load(node[:kubernetes][:databag], "#{node[:kubernetes][:cluster_name]}_#{f}")['body']
+  end
 end
