bc49f96213d59611cbdb6cfdfc38276e937eb569
Maxim Filatov <pipopolam@gmail.com>
2016-06-05 14:16:10 +0300

    Recipes::Node: drop bunch of runit services before migration to manifests

--- recipes/node.rb
+++ recipes/node.rb
@@ -13,27 +13,7 @@ internal_ip = node[:network][:interfaces][node['kubernetes']['interface']]
 if Chef::Config[:solo]
   master_ip = node['kubernetes']['master']['ip']
 else
-  master_node = search(:node, 'role:kubernetes-master').first
+  master_node = search(:node, 'role:kubernetes_master').first
   master_ip = master_node[:network][:interfaces][node['kubernetes']['interface']]
               .addresses.find {|addr, properties| properties['family'] == 'inet'}.first
-end
-
-runit_service 'kubelet' do
-  default_logger true
-  template_name 'docker'
-  options(
-    run_args: '--name=kubelet -i --rm --net=host -v /var/run/docker.sock:/var/run/docker.sock --privileged',
-    image: "gcr.io/google_containers/hyperkube:#{node['kubernetes']['version']}",
-    command: "/hyperkube kubelet --api_servers=http://#{master_ip}:8080 --v=2 --address=#{internal_ip} --enable_server --hostname_override=#{internal_ip} --allow-privileged --cluster_dns=#{node['kubernetes']['cluster_dns']} --cluster_domain=#{node['kubernetes']['cluster_domain']}"
-  )
-end
-
-runit_service 'kube_proxy' do
-  default_logger true
-  template_name 'docker'
-  options(
-    run_args: '--name=kubernetes-proxy -i --rm --net=host --privileged -v /var/run/docker.sock:/var/run/docker.sock',
-    image: "gcr.io/google_containers/hyperkube:#{node['kubernetes']['version']}",
-    command: "/hyperkube proxy --master=http://#{master_ip}:8080 --v=2"
-  )
 end
