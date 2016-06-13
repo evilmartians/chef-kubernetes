51e6c75492fc97b426cb9bae94423e6bfeaaa513
Maxim Filatov <pipopolam@gmail.com>
2016-06-13 16:39:24 +0300

    Docker installation with other packages moved to separate recipe

---
+++ recipes/packages.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: kubernetes
+# Recipe:: packages
+#
+# Author:: Maxim Filatov <bregor@evilmartians.com>
+#
+
+apt_repository 'docker' do
+  uri "https://apt.dockerproject.org/repo"
+  distribution "#{node.platform}-#{node.lsb.codename}"
+  components ['main']
+  keyserver 'hkp://p80.pool.sks-keyservers.net:80'
+  key '58118E89F3A912897C070ADBF76221572C52609D'
+end
+
+package 'iptables'
+package 'docker-engine'
+package 'socat'
+package 'ethtool'
+
+bash 'install_nsenter' do
+  code <<-EOH
+/usr/bin/docker run --rm -v /usr/local/bin:/target jpetazzo/nsenter
+chmod +x /usr/local/bin/nsenter
+/usr/bin/docker rmi jpetazzo/nsenter
+EOH
+  not_if { File.exists? '/usr/local/bin/nsenter' }
+end
+
+%w(kubelet kubectl).each do |f|
+  remote_file "/usr/local/bin/#{f}" do
+    source "https://storage.googleapis.com/kubernetes-release/release/#{node[:kubernetes][:version]}/bin/linux/amd64/#{f}"
+    mode '0755'
+    not_if { Digest::MD5.file("/usr/local/bin/#{f}").to_s == node[:kubernetes][:md5][f.to_sym] rescue false }
+  end
+end
--- recipes/system.rb
+++ recipes/system.rb
@@ -5,29 +5,13 @@
 # Author:: Maxim Filatov <bregor@evilmartians.com>
 #
 
+internal_ip = node[:network][:interfaces][node['kubernetes']['interface']]
+              .addresses.find {|addr, properties| properties['family'] == 'inet'}.first
 
-bash 'install_nsenter' do
-  code <<-EOH
-/usr/bin/docker run --rm -v /usr/local/bin:/target jpetazzo/nsenter
-chmod +x /usr/local/bin/nsenter
-/usr/bin/docker rmi jpetazzo/nsenter
-EOH
-  not_if { File.exists? '/usr/local/bin/nsenter' }
-end
+hostname = internal_ip
 
-package 'socat'
 
-%w(kubelet kubectl).each do |f|
-  remote_file "/usr/local/bin/#{f}" do
-    source "https://storage.googleapis.com/kubernetes-release/release/#{node[:kubernetes][:version]}/bin/linux/amd64/#{f}"
-    mode '0755'
-    not_if { Digest::MD5.file("/usr/local/bin/#{f}").to_s == node[:kubernetes][:md5][f.to_sym] rescue false }
-  end
-end
-
-service 'kubelet' do
-  provider node['platform_version'].to_f < 16.04 ? Chef::Provider::Service::Upstart : Chef::Provider::Service::Systemd
-  supports :restart => true
-  start_command "/usr/local/bin/kubelet --api_servers=https://#{node[:kubernetes][:master]}:#{node[:kubernetes][:secure_port]} --cluster-dns=#{node[:kubernetes][:cluster_dns]} hostname_override=#{node[:hostname]} --allow_privileged=true --config=/etc/kubernetes/manifests --kubeconfig=/etc/kubernetes/kubeconfig.yaml"
-  action [:enable, :start]
+poise_service 'kubelet' do
+  provider node['platform_version'].to_f < 16.04 ? :runit : :systemd
+  command "/usr/local/bin/kubelet --api_servers=https://#{node[:kubernetes][:master]}:#{node[:kubernetes][:api][:secure_port]} --cluster-dns=#{node[:kubernetes][:cluster_dns]} --hostname_override=#{hostname} --allow_privileged=true --config=/etc/kubernetes/manifests --kubeconfig=/etc/kubernetes/kubeconfig.yaml --log-dir=/var/log/kubernetes"
 end
