b364200f0db46f855992c4453afed0e5f02e8b92
Maxim Filatov <pipopolam@gmail.com>
2016-08-23 16:23:22 +0300

    WeaveScope-0.17.1

--- attributes/weave.rb
+++ attributes/weave.rb
@@ -6,6 +6,6 @@ default[:kubernetes][:md5][:weave]                    = '13bf4fdb93efb70f77b5c67
 default[:kubernetes][:md5][:weave_ipam]               = '48df8ba7ddf3d57818385534d76b3f5c'
 default[:kubernetes][:md5][:weave_net]                = '4f4580c35601e4d74edb55d36bc5b96c'
 
-default[:kubernetes][:weavescope][:version]           = '0.17.0'
+default[:kubernetes][:weavescope][:version]           = '0.17.1'
 default[:kubernetes][:weavescope][:master]            = ''
 default[:kubernetes][:weavescope][:port]              = '4040'
