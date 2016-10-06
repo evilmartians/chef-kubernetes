ba39940f4ef5ff0fde918cded9bbde6b1841acca
Maxim Filatov <pipopolam@gmail.com>
2016-10-06 09:38:00 +0300

    Weave: remove deprecated helpers

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -12,20 +12,5 @@ class Chef
       n.send(:kubernetes)[:register_as] == 'ip' ? internal_ip(n) : n.send(:hostname)
     end
 
-    def weave_images
-      %w(weave weaveexec plugin).map { |image| "weaveworks/#{image}:#{node[:kubernetes][:weave][:version]}" }
-    end
-
-    def weave_works?
-      weave_images.all? { |c| system("docker ps|grep '#{c}' >/dev/null 2>&1") }
-    end
-
-    def weavedb_downloaded?
-      system("docker images weaveworks/weavedb:latest|grep 'latest' >/dev/null 2>&1")
-    end
-
-    def weave_images_downloaded?
-      weave_images.all? { |image| system("docker images #{image}|grep '#{node[:kubernetes][:weave][:version]}' >/dev/null 2>&1") }
-    end
   end
 end
