be340a5dd3c0a1becb0541f38250411cdf686135
Maxim Filatov <pipopolam@gmail.com>
2016-10-13 14:57:41 +0300

    Kubeconfig: set user via variables

--- recipes/default.rb
+++ recipes/default.rb
@@ -36,7 +36,8 @@ template '/etc/kubernetes/kubeconfig.yaml' do
   if node[:kubernetes][:token_auth]
     variables(token: Chef::EncryptedDataBagItem.load(node[:kubernetes][:databag], 'users')['users']
                 .find { |user| user['name'] == 'kubelet' }['token'],
-              ca_file: Base64.encode64(ca_file).gsub(/\n/,''))
+              ca_file: Base64.encode64(ca_file).gsub(/\n/,''),
+              user: 'kubelet')
   end
 end
 
