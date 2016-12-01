5238153e9f6770004c5805efd3fd284069ef966c
Maxim Filatov <pipopolam@gmail.com>
2016-12-01 13:26:10 +0300

    Weave version set to 1.8.1

--- attributes/weave.rb
+++ attributes/weave.rb
@@ -1,4 +1,4 @@
-default['kubernetes']['weave']['version']                = '1.7.1'
+default['kubernetes']['weave']['version']                = '1.8.1'
 default['kubernetes']['weave']['network']                = '192.168.0.0/16'
 default['kubernetes']['weave']['interface']              = 'eth1'
 default['kubernetes']['weave']['use_scope']              = true
