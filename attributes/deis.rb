262f96834f712e266414729537ddd23f4ca7dda2
Maxim Filatov <pipopolam@gmail.com>
2016-12-01 13:45:40 +0300

    Deis party!

---
+++ attributes/deis.rb
@@ -0,0 +1,4 @@
+default['kubernetes']['deis']['enabled']           = false
+default['kubernetes']['deis']['route_via']         = 'haproxy'
+default['kubernetes']['deis']['builder']['port']   = 2222
+default['kubernetes']['deis']['registry']['port']  = 5555
