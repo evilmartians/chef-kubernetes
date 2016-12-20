53e00f46be1b8007504bcb44950f7ed894b1bc39
Maxim Filatov <pipopolam@gmail.com>
2016-12-21 01:05:37 +0300

    Separate recipe for kubeconfig

---
+++ recipes/kubeconfig.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: kubernetes
+# Recipe:: kubeconfig
+#
+# Author:: Maxim Filatov <bregor@evilmartians.com>
+#
+
+require 'base64'
+
+ca_file = Chef::EncryptedDataBagItem.load(node['kubernetes']['databag'], "#{node['kubernetes']['cluster_name']}_cluster_ssl")['client_ca_file']
+
+file node['kubernetes']['client_ca_file'] do
+  content ca_file
+end
+
+template '/etc/kubernetes/kubeconfig.yaml' do
+  source 'kubeconfig.yaml.erb'
+  if node['kubernetes']['token_auth']
+    variables(token: Chef::EncryptedDataBagItem.load(node['kubernetes']['databag'], 'users')['users']
+                .find { |user| user['name'] == 'kubelet' }['token'],
+              ca_file: Base64.encode64(ca_file).gsub(/\n/,''),
+              user: 'kubelet')
+  end
+end
