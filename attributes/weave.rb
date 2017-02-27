d22164aae6b081b4ad931b885ba27c13b8d6add9
Maxim Filatov <pipopolam@gmail.com>
2017-02-28 02:27:27 +0300

    Weave: 1.9.1

--- attributes/weave.rb
+++ attributes/weave.rb
@@ -1,4 +1,4 @@
-default['kubernetes']['weave']['version']                = '1.9.0'
+default['kubernetes']['weave']['version']                = '1.9.1'
 default['kubernetes']['weave']['network']                = '192.168.0.0/16'
 default['kubernetes']['weave']['interface']              = 'eth1'
 default['kubernetes']['weave']['use_scope']              = true
