592b329a7046a25a79ac31ce505080198bbad0ca
Maxim Filatov <pipopolam@gmail.com>
2017-04-23 17:28:59 +0300

    HAProxy: resolvers section

--- recipes/haproxy.rb
+++ recipes/haproxy.rb
@@ -5,6 +5,8 @@
 # Author:: Maxim Filatov <bregor@evilmartians.com>
 #
 
+require 'resolv'
+
 apt_repository 'haproxy' do
   uri 'ppa:vbernat/haproxy-1.7'
   distribution node['lsb']['codename']
@@ -20,9 +22,22 @@ master_nodes = search(:node, "role:#{node['kubernetes']['roles']['master']}")
 if !master_nodes.empty? && master_nodes.all? {|n| n.keys.include? 'kubernetes'}
   master_nodes.map! { |node| { name: hostname(node), ip: internal_ip(node) } }
 
+  nameservers = Resolv::DNS::Config.new.lazy_initialize.nameserver_port
+  nameservers = nameservers.unshift([node['kubernetes']['cluster_dns'], 53])
+  nameservers.uniq!
+
   template '/etc/haproxy/haproxy.cfg' do
     source 'haproxy.cfg.erb'
-    variables(nodes: master_nodes)
+    variables(
+      nodes:     master_nodes,
+      resolvers: [
+        { name: node['kubernetes']['cluster_name'],
+          nameservers:     nameservers,
+          resolve_retries: 3,
+          timeout_retry:   '1s'
+        }
+      ]
+    )
     notifies :restart, 'service[haproxy]'
   end
 end
