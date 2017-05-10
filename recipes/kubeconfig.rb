8fb59cbbf0bf1cacbb8d4b89868a4ba92f9360e7
Maxim Filatov <pipopolam@gmail.com>
2017-05-10 22:20:31 +0300

    Recipes: separate kubeconfig for kube-proxy Don't forget to add separate user `system:kube-proxy` in group `system:nodes`

--- recipes/kubeconfig.rb
+++ recipes/kubeconfig.rb
@@ -13,12 +13,15 @@ file node['kubernetes']['client_ca_file'] do
   content ca_file
 end
 
-template '/etc/kubernetes/kubeconfig.yaml' do
-  source 'kubeconfig.yaml.erb'
-  if node['kubernetes']['token_auth']
-    variables(token: Chef::EncryptedDataBagItem.load(node['kubernetes']['databag'], 'users')['users']
-                .find { |user| user['name'] == 'kubelet' }['token'],
-              ca_file: Base64.encode64(ca_file).delete("\n"),
-              user: 'kubelet')
+['kubelet', 'system:kube-proxy'].each do |username|
+  template "/etc/kubernetes/#{username}_config.yaml" do
+    source 'kubeconfig.yaml.erb'
+    if node['kubernetes']['token_auth']
+      users = Chef::EncryptedDataBagItem.load(node['kubernetes']['databag'], 'users')['users']
+      token = users.find { |user| user['name'] == username }['token']
+      variables(token: token,
+                ca_file: Base64.encode64(ca_file).delete("\n"),
+                user: username)
+    end
   end
 end
