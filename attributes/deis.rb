7accd183ce9a02a75600d0773e4a471667400b2f
Maxim Filatov <pipopolam@gmail.com>
2017-05-27 23:02:52 +0300

    HAProxy: removed deis_registry Starting from workflow-2.14 deis-registry-proxy is CNI-compatible

--- attributes/deis.rb
+++ attributes/deis.rb
@@ -1,4 +1,3 @@
 default['kubernetes']['deis']['enabled']           = false
 default['kubernetes']['deis']['route_via']         = 'haproxy'
 default['kubernetes']['deis']['builder']['port']   = 2222
-default['kubernetes']['deis']['registry']['port']  = 5555
