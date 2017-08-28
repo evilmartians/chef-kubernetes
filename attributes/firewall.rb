57297f817a9b434acdb6a26e97f4c237778eb3f4
Maxim Filatov <pipopolam@gmail.com>
2017-08-28 18:36:32 +0300

    Firewall: default settings

---
+++ attributes/firewall.rb
@@ -0,0 +1,6 @@
+default['firewall']['allow_ssh']               = true
+default['firewall']['allow_loopback']          = true
+default['firewall']['allow_icmp']              = true
+default['firewall']['ubuntu_iptables']         = false
+default['firewall']['allow_established']       = true
+default['firewall']['ipv6_enabled']            = true
