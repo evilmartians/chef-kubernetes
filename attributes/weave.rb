1cdf95a400305d8b8ff675687c6f59a44a9c617a
Maxim Filatov <pipopolam@gmail.com>
2017-09-22 20:20:46 +0300

    Firewall: allow input from pod network

--- attributes/weave.rb
+++ attributes/weave.rb
@@ -3,5 +3,6 @@ default['kubernetes']['weave']['network']                = '192.168.0.0/16'
 default['kubernetes']['weave']['interface']              = 'weave'
 default['kubernetes']['weave']['use_scope']              = true
 default['kubernetes']['weave']['use_portmap']            = false
+default['kubernetes']['weave']['interface']              = 'weave'
 default['kubernetes']['weavescope']['version']           = '0.17.1'
 default['kubernetes']['weavescope']['port']              = '4040'
