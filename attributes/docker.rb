69e46b893d0745f76991a70fa04753ad246ba7dc
Maxim Filatov <pipopolam@gmail.com>
2017-09-28 16:40:13 +0300

    Attributes::Docker: turn off iptables management by docker daemon

--- attributes/docker.rb
+++ attributes/docker.rb
@@ -1,3 +1,5 @@
 default['docker']['version']                    = '1.12.6-0'
 default['docker']['settings']['storage-driver'] = 'aufs'
 default['docker']['settings']['live-restore']   = true
+default['docker']['settings']['iptables']       = false
+default['docker']['settings']['ip-masq']        = false
