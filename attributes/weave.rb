c89f885e1badec6fd9511d0c00d19fab7b3e4721
Maxim Filatov <pipopolam@gmail.com>
2017-10-04 12:48:59 +0300

    Weave: set .spec.updateStrategy.type by attribute

--- attributes/weave.rb
+++ attributes/weave.rb
@@ -1,8 +1,8 @@
-default['kubernetes']['weave']['version']                = '2.0.4'
-default['kubernetes']['weave']['network']                = '192.168.0.0/16'
-default['kubernetes']['weave']['interface']              = 'weave'
-default['kubernetes']['weave']['use_scope']              = true
-default['kubernetes']['weave']['use_portmap']            = true
-default['kubernetes']['weave']['interface']              = 'weave'
-default['kubernetes']['weavescope']['version']           = '0.17.1'
-default['kubernetes']['weavescope']['port']              = '4040'
+default['kubernetes']['weave']['version']                 = '2.0.4'
+default['kubernetes']['weave']['network']                 = '192.168.0.0/16'
+default['kubernetes']['weave']['interface']               = 'weave'
+default['kubernetes']['weave']['use_scope']               = true
+default['kubernetes']['weave']['use_portmap']             = true
+default['kubernetes']['weave']['update_strategy']['type'] = 'RollingUpdate'
+default['kubernetes']['weavescope']['version']            = '0.17.1'
+default['kubernetes']['weavescope']['port']               = '4040'
