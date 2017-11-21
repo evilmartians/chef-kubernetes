df5664aaac4726b499822b7a054dfd75c28f5d8d
Maxim Filatov <pipopolam@gmail.com>
2017-11-22 02:43:29 +0300

    Authorization: default modes set to Node,RBAC

--- attributes/authorization.rb
+++ attributes/authorization.rb
@@ -1,5 +1,5 @@
 default['kubernetes']['authorization']['admin_groups'] = ['admins']
-default['kubernetes']['authorization']['mode']         = 'AlwaysAllow' # Available variants are: AlwaysAllow,AlwaysDeny,ABAC,RBAC,Node
+default['kubernetes']['authorization']['mode']         = 'Node,RBAC' # Available variants are: AlwaysAllow,AlwaysDeny,ABAC,RBAC,Node
 default['kubernetes']['authorization']['policies']     = [
   { user:  '*',                                                                    nonResourcePath: '*', readonly: true },
   { user:  'admin',                  namespace: '*', resource: '*', apiGroup: '*' },
