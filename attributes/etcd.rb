9e59a8ecd2f77bc784e7c4f87715eecde6cbb192
Maxim Filatov <pipopolam@gmail.com>
2017-12-07 17:17:19 +0300

    Etcd: 3.2.11

--- attributes/etcd.rb
+++ attributes/etcd.rb
@@ -1,5 +1,5 @@
 default['etcd']['discovery_url']         = ''
-default['etcd']['version']               = 'v3.2.10'
+default['etcd']['version']               = 'v3.2.11'
 default['etcd']['image']                 = 'quay.io/coreos/etcd'
 default['etcd']['trusted_ca_file']       = '/etc/kubernetes/ssl/ca.pem'
 default['etcd']['client_cert_auth']      = 'true'
