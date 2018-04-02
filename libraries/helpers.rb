d4b2767585e1f3537ad3314a24833ff88fa50bd5
gfrntz <chainwolf@gmail.com>
2018-04-02 23:28:15 +0300

    fix cookstyle for helpers.rb

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -1,7 +1,6 @@
 module Kubernetes
   # Few helpers for Kubernetes installation.
   module Helpers
-
     def k8s_ip(n = node)
       n['network']['interfaces'][n['kubernetes']['interface']]['addresses']
         .find { |_addr, data| data['family'] == 'inet' }.first
@@ -9,11 +8,9 @@ module Kubernetes
       '127.0.0.1'
     end
 
-
     def k8s_hostname(n = node)
       n['kubernetes']['register_as'] == 'ip' ? k8s_ip(n) : n['hostname']
     end
-
 
     def install_via(n = node)
       result = n['kubernetes']['install_via']
@@ -24,15 +21,13 @@ module Kubernetes
       result
     end
 
-
     def kubelet_args
-      options = Hash[node['kubernetes']['kubelet'].map { |k, v| [k.gsub(/_/, '-'), v] } ]
+      options = Hash[node['kubernetes']['kubelet'].map { |k, v| [k.tr('_', '-'), v] }]
       options.store('address', k8s_ip(node)) # FIXME
       options.store('hostname-override', k8s_hostname(node)) #  FIXME
       options.store('node-ip', k8s_ip(node)) # FIXME
-      options.sort_by{ |k,v| k}.map{|k,v| v.nil? ? "--#{k}" : "--#{k}=#{v}"}
+      options.sort_by { |k, v| k }.map { |k, v| v.nil? ? "--#{k}" : "--#{k}=#{v}" }
     end
-
 
     def apiserver_args
       etcd_nodes = search(
@@ -48,44 +43,42 @@ module Kubernetes
         etcd_servers = "#{node['etcd']['proto']}://#{k8s_ip(node)}:#{node['etcd']['client_port']}"
       end
 
-      options = Hash[node['kubernetes']['api'].map { |k, v| [k.gsub(/_/, '-'), v] } ]
-      options.store('advertise-address',k8s_ip(node)) # FIXME
-      options.store('etcd-servers',etcd_servers) # FIXME
+      options = Hash[node['kubernetes']['api'].map { |k, v| [k.tr('_', '-'), v] }]
+      options.store('advertise-address', k8s_ip(node)) # FIXME
+      options.store('etcd-servers', etcd_servers) # FIXME
 
       if node['kubernetes']['api']['endpoint_reconciler_type'] == 'master-count'
         master_nodes = search(:node, "roles:#{node['kubernetes']['roles']['master']}")
         master_nodes = [node] if master_nodes.empty?
-        options.store('apiserver-count',master_nodes.size)
+        options.store('apiserver-count', master_nodes.size)
       end
 
       if node['kubernetes']['token_auth']
-        options.store('token-auth-file',node['kubernetes']['token_auth_file'])
+        options.store('token-auth-file', node['kubernetes']['token_auth_file'])
       end
 
       if node['kubernetes']['authorization']['mode'].include?('ABAC')
-        options.store('authorization-policy-file','/etc/kubernetes/authorization-policy.jsonl')
+        options.store('authorization-policy-file', '/etc/kubernetes/authorization-policy.jsonl')
       end
 
       if node['kubernetes']['audit']['enabled']
-        options.store('audit-log-maxbackup',node['kubernetes']['audit']['maxbackup'])
+        options.store('audit-log-maxbackup', node['kubernetes']['audit']['maxbackup'])
         options.store('audit-log-path', node['kubernetes']['audit']['log_file'])
       end
 
-      options.sort_by{ |k,v| k}.map{|k,v| v.nil? ? "--#{k}" : "--#{k}=#{v}"}
+      options.sort_by { |k, v| k }.map { |k, v| v.nil? ? "--#{k}" : "--#{k}=#{v}" }
     end
-
 
     def controller_manager_args
-      options = Hash[node['kubernetes']['controller_manager'].map { |k, v| [k.gsub(/_/, '-'), v] } ]
+      options = Hash[node['kubernetes']['controller_manager'].map { |k, v| [k.tr('_', '-'), v] }]
 
       if node['kubernetes']['sdn'] == 'canal'
-        options.store('allocate-node-cidrs',nil)
-        options.store('node-cidr-mask-size',node['kubernetes']['node_cidr_mask_size'])
+        options.store('allocate-node-cidrs', nil)
+        options.store('node-cidr-mask-size', node['kubernetes']['node_cidr_mask_size'])
       end
 
-      options.sort_by{ |k,v| k}.map{|k,v| v.nil? ? "--#{k}" : "--#{k}=#{v}"}
+      options.sort_by { |k, v| k }.map { |k, v| v.nil? ? "--#{k}" : "--#{k}=#{v}" }
     end
-
   end
 end
 
