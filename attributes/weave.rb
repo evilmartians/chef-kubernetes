b2b5b4581f3ff97bfab3d26e6aafb56a80419229
Maxim Filatov <pipopolam@gmail.com>
2018-04-14 17:11:09 +0300

    Weave: 2.3.0
    - split `--status-addr` to separate `--status-addr` and
      `--metrics-addr` args

--- attributes/weave.rb
+++ attributes/weave.rb
@@ -1,4 +1,4 @@
-default['kubernetes']['weave']['version']                 = '2.2.1'
+default['kubernetes']['weave']['version']                 = '2.3.0'
 default['kubernetes']['weave']['interface']               = 'weave'
 default['kubernetes']['weave']['use_scope']               = true
 default['kubernetes']['weave']['use_portmap']             = true
