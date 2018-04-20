25c1d11332f7597277604108bcaa8ab156bcfa8d
Maxim Filatov <pipopolam@gmail.com>
2018-04-20 12:15:44 +0300

    Haproxy: sort master nodes to prevent config reload on each run

--- recipes/haproxy.rb
+++ recipes/haproxy.rb
@@ -45,7 +45,7 @@ end
 template '/etc/haproxy/haproxy.cfg' do
   source 'haproxy.cfg.erb'
   variables(
-    nodes:     master_nodes,
+    nodes:     master_nodes.sort_by { |h| h[:ip] },
     resolvers: [
       {
         name: node['kubernetes']['cluster_name'],
