a4f9e2cccee109f0b9f0b8ce96cf7bcea96ea750
Maxim Filatov <pipopolam@gmail.com>
2018-12-13 17:39:12 +0300

    Version bump to 1.13.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'bregor@evilmartians.com'
 license          'MIT'
 description      'Installs/Configures google kubernetes'
 long_description 'Google Kubernetes installer for Ubuntu'
-version          '1.13.0'
+version          '1.13.1'
 
 chef_version     '>= 12.14'
 
