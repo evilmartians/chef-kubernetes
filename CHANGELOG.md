8cac1fe0a2f54ba956a2bf33af410800c3510a8e
Maxim Filatov <pipopolam@gmail.com>
2019-04-17 17:41:35 +0300

    buildah: 1.7.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # UNRELEASED
+- [buildah: 1.7.3](https://github.com/containers/buildah/releases/tag/v1.7.3)
 - [cri-o: 1.13.4](https://github.com/kubernetes-sigs/cri-o/releases/tag/v1.13.4)
 - [podman: 1.2.0](https://github.com/containers/libpod/releases/tag/v1.2.0)
 - [skopeo: 0.1.35](https://github.com/containers/skopeo/releases/tag/v0.1.35)
--- attributes/crio.rb
+++ attributes/crio.rb
@@ -1,7 +1,7 @@
 default['kubernetes']['crio']['version']                     = '1.13.4'
 default['kubernetes']['crio']['endpoint']                    = '/var/run/crio/crio.sock'
 default['kubernetes']['crio']['endpoint_proto']              = ''
-default['kubernetes']['buildah']['version']                  = '1.7.1'
+default['kubernetes']['buildah']['version']                  = '1.7.3'
 default['kubernetes']['libpod']['version']                   = '1.2.0'
 default['kubernetes']['skopeo']['version']                   = '0.1.35'
 
@@ -9,13 +9,13 @@ default['kubernetes']['checksums']['skopeo']['bionic']       = '89cae23860e0b089
 default['kubernetes']['checksums']['podman']['bionic']       = '6cf14a3e4e334ca057981b0e4df208d8e922906379a0dab74c1d2ff0200895fd'
 default['kubernetes']['checksums']['conmon']['bionic']       = '6de5bf7310c25d13ebe97f48ff902a85bf2830999aa61faa76c290be75b5b592'
 default['kubernetes']['checksums']['crio']['bionic']         = '37c6f48f825ec8684a8f6b4f1908c9e1557b10e0a21f48b2654a9a7cb8275613'
-default['kubernetes']['checksums']['buildah']['bionic']      = 'b289588f6c3073b797b6683344f772becbe5686085335096cd47b5fd1d7d7bff'
+default['kubernetes']['checksums']['buildah']['bionic']      = 'b84ca4afcd0563510e5c30be07219464997f3257eb9e5351bac6c1afd8dd764a'
 
 default['kubernetes']['checksums']['skopeo']['xenial']       = '3b94edd39c7d875bfd5c11be867f99ee74e415dc944a4cf41a6c1429d913c4e1'
 default['kubernetes']['checksums']['podman']['xenial']       = '7d16a9da7a976dab35c9244166e0c9393cd561514a44f5252466c72501f62757'
 default['kubernetes']['checksums']['conmon']['xenial']       = 'e8016bf8fcbaf1faeb7c98fce375993068b601799d787a803a4713429d635809'
 default['kubernetes']['checksums']['crio']['xenial']         = 'c7c7f28ab844740d77d05c21d7c78cb81276f5c81957808d59698fbf1f12ebb4'
-default['kubernetes']['checksums']['buildah']['xenial']      = '36a92aebecfdc3eb282f6fc8d7aa003031fa4e30f7a980f62dfeaba64f6075d0'
+default['kubernetes']['checksums']['buildah']['xenial']      = 'fdfa31b438331a8f2ed96db8663deba82624d97f537f053955db86f213f11368'
 
 default['kubernetes']['crio']['config']['conmon']            = '/usr/local/bin/conmon'
 default['kubernetes']['crio']['config']['storage_driver']    = 'overlay'
